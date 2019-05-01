d053799f605de33f9f2f0522e67b3c6f71cf1ed0
Jahon <worldj95@gmail.com>
2019-04-30 22:05:00 -0700

    Initial Commit

---
+++ Just Do It/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Just Do It
+//
+//  Created by Jakhongir Khusanov on 4/30/19.
+//  Copyright © 2019 Jakhongir Khusanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
