52102e7d7d93cbba784b4b5db02cf5dd3ac673ef
Jahon <worldj95@gmail.com>
2019-05-01 21:14:11 -0700

    Add sections and display dummy data

--- Just Do It/TableController.swift
+++ Just Do It/TableController.swift
@@ -10,18 +10,46 @@ import UIKit
 
 class TableController: UITableViewController {
     
-    // TODO: Create tasks
+    var taskStore = TaskStore()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad() // calls tableViewController's viewDidLoad method
+        
+        let uncompletedTasks = [Task(name: "Buy meat"), Task(name: "Buy bread"), Task(name: "Cook food")]
+        let completedTasks = [Task(name: "Do homework")]
+        
+        taskStore.tasks = [uncompletedTasks, completedTasks]
+    }
+    
+  
     
 }
 
 // MARK: - DataSource
 extension TableController {
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "TO-DO"
+        } else {
+            return "Done"
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 60
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return taskStore.tasks.count
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return taskStore.tasks[section].count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "TODO"
+        cell.textLabel?.text = taskStore.tasks[indexPath.section][indexPath.row].name
         return cell
     }
 }
